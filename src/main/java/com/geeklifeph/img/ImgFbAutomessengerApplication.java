727fdbec543dc35bad74936d9aa69ee1b7ac10d9
Allan Cayanan <geeklifeph@gmail.com>
2018-05-16 17:42:21 +0800

    First commit - base springboot app

---
+++ src/main/java/com/geeklifeph/img/ImgFbAutomessengerApplication.java
@@ -0,0 +1,12 @@
+package com.geeklifeph.img;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgFbAutomessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgFbAutomessengerApplication.class, args);
+	}
+}
